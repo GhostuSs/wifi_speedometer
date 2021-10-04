4e748b08f67766ddd4cede59e3e432c9af03b674
Maxim Movchan <58935940+GhostuSs@users.noreply.github.com>
2021-10-05 00:00:56 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ Future<void> main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   seen = prefs.getBool("seen") ?? false;
   await prefs.setBool("seen", true);
-  seen=false;
   runApp(
     MultiProvider(
       providers: [
